9b8c3ebd85ce7c5ecdff01f39ff064133a18d921
Pablo Egido <pgarciaegido@gmail.com>
2017-09-28 21:36:48 +0200

    Added Hello World

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,22 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 
 server.connection({
-    host: 'localhost',
-    port: 5000
+	host: 'localhost',
+	port: 5000
 });
-console.log('hola');
+
+server.route({
+	method: 'GET',
+	path: '/hello',
+	handler(req, res) {
+		return res('Hello!!');
+	}
+});
+
+server.start((err) => {
+	if (err) {
+		console.log('There has been an error');
+	}
+
+	console.log(`Server running at: ${server.info.uri}`);
+});

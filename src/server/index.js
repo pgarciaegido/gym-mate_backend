e1a882d3ed8c80e2430bba2858b95cdf0c5c56c3
Pablo García Egido <pgarciaegido@gmail.com>
2017-11-18 13:25:23 +0100

    Serving documentation on /documentation :muscle:

--- src/server/index.js
+++ src/server/index.js
@@ -6,13 +6,27 @@ const { loginRouter } = require('../api/login/router/loginRouter');
 const { signupRouter } = require('../api/signup/router/signupRouter');
 const { askForPasswordRecovery, changePasswordGet, changePasswordPost } = require('../api/passwordRecovery/router/passwordRecoveryRouter');
 
+// Register inert plugin
+server.register(require('inert'));
+
 // Connection
 server.connection({
     host: 'localhost',
     port: 5000
 });
 
-// Routes
+// Serving static docs.
+server.route({
+    method: 'GET',
+    path: '/documentation/{file*}',
+    handler: {
+        directory: {
+            path: 'docs'
+        }
+    }
+});
+
+// Router
 server.route(loginRouter);
 server.route(signupRouter);
 server.route(askForPasswordRecovery);
